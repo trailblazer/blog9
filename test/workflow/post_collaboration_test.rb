c0ca96f8bd2aea110ef32462d558fefaa8372980
Nick Sutterer <apotonick@gmail.com>
2020-12-21 08:48:22 +0100

    simplification in the test. Need to extract that to endpoint/test.

--- test/workflow/post_collaboration_test.rb
+++ test/workflow/post_collaboration_test.rb
@@ -5,24 +5,36 @@ class PostCollaborationTest < Minitest::Spec
 
   let(:moment) { Trailblazer::Workflow::Moment::DSL }
 
+  # TODO: abstract to endpoint/test
+  def ctx_for(event_name, params: {}, process_model: nil, activity: Workflow::Collaboration::WriteWeb)
+    domain_ctx = {params: params}
+
+    ctx = {activity: activity, event_name: event_name, process_model: process_model, domain_ctx: domain_ctx} # TODO: require domain_ctx if scoping on.
+  end
+
+  # TODO: abstract to endpoint/test
+  def args_for(*args)
+    [ctx_for(*args), {context_options: {}, throw: []}]
+  end
+
+  # TODO: abstract to endpoint/test
+  # DISCUSS: this assumes that we have an Endpoint/Advance ctx here.
+  def assert_position(ctx, *args, collaboration: Workflow::Collaboration::WriteWeb)
+    super(ctx[:returned][0].to_h, *args, collaboration: collaboration)
+  end
+
   it "what" do
     endpoint = Trailblazer::Workflow.Advance(activity: Workflow::Collaboration::WriteWeb)
 
-    domain_ctx = {params: {}}
-    ctx = {activity: Workflow::Collaboration::WriteWeb, event_name: "web:new?", process_model: nil, domain_ctx: domain_ctx} # TODO: require domain_ctx if scoping on.
 
   # invalid Create data
-    signal, (ctx, _) = Trailblazer::Developer.wtf?(endpoint, [ctx, {context_options: {}, throw: []}])
+    signal, (ctx, _) = Trailblazer::Developer.wtf?(endpoint, args_for("web:new?"))
 
-    assert_position ctx[:returned][0].to_h, moment.before("catch-before-?Create!"), moment.before("new?"), collaboration: Workflow::Collaboration::WriteWeb
-
+    assert_position ctx, moment.before("catch-before-?Create!"), moment.before("new?")
 
   # processable Create data
-    domain_ctx = {params: {post: {content: "That was great!"}}}
-    ctx = {activity: Workflow::Collaboration::WriteWeb, event_name: "web:new?", process_model: nil, domain_ctx: domain_ctx}
+    signal, (ctx, _) = Trailblazer::Developer.wtf?(endpoint, args_for("web:new?", params: {post: {content: "That was great!"}}))
 
-    signal, (ctx, _) = Trailblazer::Developer.wtf?(endpoint, [ctx, {context_options: {}, throw: []}])
-
-    assert_position ctx[:returned][0].to_h, moment.before("catch-before-?Update!", "catch-before-?Notify approver!"), moment.before("edit_form?", "request_approval?"), collaboration: Workflow::Collaboration::WriteWeb
+    assert_position ctx, moment.before("catch-before-?Update!", "catch-before-?Notify approver!"), moment.before("edit_form?", "request_approval?")
   end
 end
