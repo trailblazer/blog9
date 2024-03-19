58219c0a38d6cd1457f12cc3cce210b35223a08d
Nick Sutterer <apotonick@gmail.com>
2024-03-19 08:35:56 +0100

    swap `assert_equal` args.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 require "minitest/spec"
-require "trailblazer/workflow/testing" # {#assert_position}
+# require "trailblazer/workflow/testing" # {#assert_position}
 
 class Minitest::Spec
   # # Run tests in parallel with specified workers
@@ -14,4 +14,8 @@ class Minitest::Spec
   # Add more helper methods to be used by all tests here...
 
   include Trailblazer::Test::Assertions
+
+  def assert_equal(actual, expected, *args)
+    super(expected, actual, *args)
+  end
 end
